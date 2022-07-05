115b3680c2996f2791901bc8398e7db8952a98f4
Heri Setyawan <herisetyawan233@gmail.com>
2022-07-05 19:09:04 +0700

    section skill

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Skill() {
   return (
-    <div className='h-96 w-full relative bg-red-400'>Skill</div>
+    <div className='h-96 w-full relative'>Skill</div>
   )
 }
 
